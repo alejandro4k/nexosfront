f82be2ebc16209ffe3903697188f3ec385a79867
Jhoan Alejandro Anaya <jaanaya88@misena.edu.co>
2020-03-12 14:56:27 -0500

    Updates

---
+++ precache-manifest.d1422240cd38de2ffe9e01b255e622b7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aed63893c06b098258b48e588cb39f01",
+    "url": "/Nexos/index.html"
+  },
+  {
+    "revision": "50ffb3783c78a5fdd9ec",
+    "url": "/Nexos/static/css/main.4e3e195d.chunk.css"
+  },
+  {
+    "revision": "fe2f1ff4ce09c08f2537",
+    "url": "/Nexos/static/js/2.30c482a2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Nexos/static/js/2.30c482a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50ffb3783c78a5fdd9ec",
+    "url": "/Nexos/static/js/main.7e72cf59.chunk.js"
+  },
+  {
+    "revision": "692a11013b102b216fe4",
+    "url": "/Nexos/static/js/runtime-main.44c0acf4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Nexos/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "86d8b95286e7f140758e1e94d5819256",
+    "url": "/Nexos/static/media/nexosLogo.86d8b952.gif"
+  }
+]);
